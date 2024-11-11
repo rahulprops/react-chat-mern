22656ec67f76f0f3de368926aaa9a5b056b0facc
rahulprops <rahulhack912@gmail.com>
2024-11-11 22:43:50 +0530

    create sidebar and workflow files

--- frontend/src/Components/MainContainer.jsx
+++ frontend/src/Components/MainContainer.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import Sidebar from './Sidebar'
+import WorkArea from './WorkArea'
 
 const MainContainer = () => {
   return (
-    <div>MainContainer</div>
+    <div style={{ display: 'flex', width: '100%' }}>
+      <div style={{ width: '25%' }}>
+        <Sidebar />
+      </div>
+      <div style={{ width: '75%' }}>
+        <WorkArea />
+      </div>
+    </div>
   )
 }
 
