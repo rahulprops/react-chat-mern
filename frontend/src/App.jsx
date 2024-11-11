877a3887490988b557a6bc5329913613b50fdf47
rahulprops <rahulhack912@gmail.com>
2024-11-11 22:22:34 +0530

    create maincontainer file

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,15 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import MainContainer from './Components/MainContainer'
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
     <>
-      <div className=' text-4xl'> react chat </div>
+      <div>
+        <MainContainer/>
+      </div>
     </>
   )
 }
