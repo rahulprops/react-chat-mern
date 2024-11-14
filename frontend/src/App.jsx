39bf17a015a411310a42ef93d3ed5d05871229ff
rahulprops <rahulhack912@gmail.com>
2024-11-14 15:54:03 +0530

    create ProtactRoute login and home

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,26 +1,34 @@
-import { lazy, useState } from 'react'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { lazy, useState } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import ProtactRoute from "./Components/auth/ProtactRoute";
 
-
-const MainContainer =lazy(()=>import('./Components/MainContainer'))
-const Login=lazy(()=>import('./pages/Login'))
-const Chat=lazy(()=>import('./pages/Chat'))
-const Group=lazy(()=>import('./pages/Group'))
+const MainContainer = lazy(() => import("./Components/MainContainer"));
+const Login = lazy(() => import("./pages/Login"));
+const Chat = lazy(() => import("./pages/Chat"));
+const Group = lazy(() => import("./pages/Group"));
 
 function App() {
-  
-
+  const user = true;
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<MainContainer/>} />
-        <Route path='/login' element={<Login/>} />
-        <Route path='/chat/:id' element={<Chat/>} />
-        <Route path='/groups' element={<Group/>} />
+        <Route element={<ProtactRoute user={user} />}>
+          <Route path="/" element={<MainContainer />} />
 
+          <Route path="/chat/:id" element={<Chat />} />
+          <Route path="/groups" element={<Group />} />
+        </Route>
+        <Route
+          path="/login"
+          element={
+            <ProtactRoute user={!user} redirect="/">
+              <Login />
+            </ProtactRoute>
+          }
+        />
       </Routes>
     </BrowserRouter>
-  )
+  );
 }
 
-export default App
+export default App;
