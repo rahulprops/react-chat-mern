2a5139bdb42479ab78445d4e4cb66cd2f4c87593
rahulprops <rahulhack912@gmail.com>
2024-11-18 11:55:26 +0530

    install cors

--- backend/Server.js
+++ backend/Server.js
@@ -1,23 +1,24 @@
-import dotenv from 'dotenv'
-import express from 'express'
-import Dbconnect from './server/Config/Dbconnection.js';
-import userRout from './server/Routers/UserRout.js';
-import bodyParser from 'body-parser';
-import messageRout from './server/Routers/messaeRout.js';
-import cookieParser from 'cookie-parser';
-import userhandler from './server/Routers/userHandlerRoute.js';
+import dotenv from "dotenv";
+import express from "express";
+import Dbconnect from "./server/Config/Dbconnection.js";
+import userRout from "./server/Routers/UserRout.js";
+import bodyParser from "body-parser";
+import messageRout from "./server/Routers/messaeRout.js";
+import cookieParser from "cookie-parser";
+import userhandler from "./server/Routers/userHandlerRoute.js";
+import cors from 'cors'
+dotenv.config();
+const app = express();
 
-  dotenv.config()
-const app=express()
+const port = process.env.PORT || 3000;
+app.use(cookieParser());
+app.use(cors())
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use("/api/user/", userRout);
+app.use("/api/user", messageRout);
+app.use("/api/userhandler", userhandler);
 
-const port=process.env.PORT || 3000;
-app.use(cookieParser())
-app.use(bodyParser.urlencoded({extended:false}))
- app.use("/api/user/",userRout)
- app.use("/api/user",messageRout)
- app.use("/api/userhandler",userhandler)
-
-app.listen(port,()=>{
-    console.log(`server start on port http://locathost:${port}`)
-    Dbconnect()
-})
+app.listen(port, () => {
+  console.log(`server start on port http://locathost:${port}`);
+  Dbconnect();
+});
