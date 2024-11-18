37afaad10ef6bbff954244c8930a3907f6e2d792
rahulprops <rahulhack912@gmail.com>
2024-11-18 13:51:19 +0530

    create multer utils

--- backend/server/Routers/UserRout.js
+++ backend/server/Routers/UserRout.js
@@ -1,11 +1,12 @@
 import { Router } from "express";
 import { logout, userLogin, userRegister } from "../Controllor/userConroller.js";
 import { AuthLogin } from "../middleware/AuthLogin.js";
+import upload from "../utils/Multer.js";
 
 const router=Router()
 
-router.post("/register",userRegister)
-router.post("/login" , userLogin)
+router.post("/register",upload.none(),userRegister)
+router.post("/login" ,upload.none(), userLogin)
 router.get("/logout",AuthLogin,logout)
 
 export default router;
